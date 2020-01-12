935438e511a8b70defabae772c97be065c044161
Grady Vickery <k.vickery@ymail.com>
2020-01-12 15:26:51 -0600

    Hook up all labels and field image to Controller. Add animation to play label. Add functionality to handle and present play results.

--- DiceBaseball/Models/SupportFunctions.swift
+++ DiceBaseball/Models/SupportFunctions.swift
@@ -21,3 +21,7 @@ import Foundation
         return [leftDiceRoll, rightDiceRoll]
     }
 
+    func delay(_ seconds: Double, completion: @escaping ()->Void) {
+           DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: completion)
+       }
+
--- DiceBaseball/ViewControllers/MainViewController.swift
+++ DiceBaseball/ViewControllers/MainViewController.swift
@@ -14,16 +14,32 @@ class MainViewController: UIViewController {
     let awayTeam: Team
     let homeTeam: Team
     var currentBatter: Player
-    var inningHalf = "top"
-
+    
+    
+    var awayBatterIndex = 0
+    var homeBatterIndex = 0
+    
+    var awayTeamScore = 0
+    var homeTeamScore = 0
+    var inningHalf = "Top"
+    var inning = 1
+    var outs = 0
+    
+    @IBOutlet weak var awayTeamScoreLabel: UILabel!
+    @IBOutlet weak var homeTeamScoreLabel: UILabel!
+    @IBOutlet weak var inningLabel: UILabel!
+    @IBOutlet weak var atBatLabel: UILabel!
+    @IBOutlet weak var outsLabel: UILabel!
+    @IBOutlet weak var playResultLabel: UILabel!
+    @IBOutlet weak var secondaryPlayLabel: UILabel!
     @IBOutlet weak var leftDiceImage: UIImageView!
     @IBOutlet weak var rightDiceImage: UIImageView!
-    
+    @IBOutlet weak var fieldImage: UIImageView!
     
     required init?(coder: NSCoder) {
         self.awayTeam = league.teams[0]
         self.homeTeam = league.teams[1]
-        self.currentBatter = awayTeam.players[0]
+        self.currentBatter = awayTeam.players[awayBatterIndex]
         super.init(coder: coder)
    }
     
@@ -32,14 +48,77 @@ class MainViewController: UIViewController {
         league.assignTeamNames()
         leftDiceImage.alpha = 0
         rightDiceImage.alpha = 0
+        
+        inningLabel.text = inningHalf + " " + String(inning)
+        playResultLabel.text = ""
+        secondaryPlayLabel.text = ""
+        atBatLabel.text = currentBatter.lastName + " " + currentBatter.position.rawValue
     }
     
-
+    
+    // MARK:- Gameplay
     @IBAction func rollTapped(_ sender: Any) {
-        let rollArray = rollDice()
+ 
+        playResultLabel.alpha = 1
+        secondaryPlayLabel.alpha = 1
         
+        let rollArray = rollDice()
         leftDiceImage.image = UIImage(named: "Dice\(rollArray[0])")
         rightDiceImage.image = UIImage(named: "Dice\(rollArray[1])")
+        animateDice()
+    
+        playResultLabel.text = currentBatter.getDiceResults(dice1: rollArray[0], dice2: rollArray[1]).rawValue
+        secondaryPlayLabel.text = "Ready for next batter"
+        animatePlayLabelsIn()
+        
+        // TODO:- handle after at bat
+        // will need to check outs, inning, team, score before call
+        delay(6.0) {
+            self.nextBatter()
+            self.animatePlayLabelsOut() // Fading out currently
+        }
+    }
+    
+    func nextBatter() {
+        // *** WIll need to determine home or away roster
+        // *** Need to check inning state: Outs, top/bottom, etc..
+        // change to incorporate 3 outs and change to opponent roster
+        if awayBatterIndex < awayTeam.players.count {
+            awayBatterIndex += 1
+        } else {
+            awayBatterIndex = 0
+        }
+   
+        currentBatter = awayTeam.players[awayBatterIndex]
+        atBatLabel.text = currentBatter.lastName + " " + currentBatter.position.rawValue
+    }
+
+    // MARK:- Animations
+    func animatePlayLabelsIn() {
+        playResultLabel.center.x -= view.bounds.width
+        secondaryPlayLabel.center.x += view.bounds.width
+        UIView.animate(withDuration: 0.5, delay: 0.9, options: [], animations:  {
+            self.playResultLabel.center.x += self.view.bounds.width
+        }, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 3, options: [], animations: {
+            self.secondaryPlayLabel.center.x -= self.view.bounds.width
+        }, completion: nil)
+        
+        
+    }
+    func animatePlayLabelsOut() {
+        
+        // Fades labels off screen
+        UIView.animate(withDuration: 0.6, animations: {
+            self.playResultLabel.alpha = 0
+        })
+        UIView.animate(withDuration: 1.1, animations: {
+            self.secondaryPlayLabel.alpha = 0
+        })
+    }
+    
+    func animateDice() {
+        // Before dice animation setup
         leftDiceImage.transform = CGAffineTransform(rotationAngle: -.pi / 2)
         rightDiceImage.transform = CGAffineTransform(rotationAngle: .pi + .pi / 2)
         leftDiceImage.center.x -= 50
@@ -48,7 +127,6 @@ class MainViewController: UIViewController {
         rightDiceImage.center.y += 300
         leftDiceImage.alpha = 1
         rightDiceImage.alpha = 1
-        
         
         //animate dice in
         UIView.animate(withDuration: 0.5, delay: 0.3, usingSpringWithDamping: 0.65, initialSpringVelocity: 0.8, options: [], animations: {
@@ -59,10 +137,9 @@ class MainViewController: UIViewController {
             self.rightDiceImage.center.y -= 300
             self.rightDiceImage.transform = CGAffineTransform(rotationAngle: -.pi - .pi / 2)
         }, completion: nil)
-        
     }
     
-    // segue to player card
+    // MARK:- Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
          if segue.identifier == "ShowPlayer" {
             let controller = segue.destination as! BaseballCardViewController
